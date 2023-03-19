d4ae2387371230d0c2c7ab9c8275b7ec411e4b75
Leonardo Nerone <leofnerone@proton.me>
2023-03-19 19:06:58 -0400

    add(lib): add initial src/error.rs with Io and alias Result

---
+++ src/error.rs
@@ -0,0 +1,10 @@
+use std::io;
+use thiserror::Error;
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("I/O error: {0}")]
+    Io(#[from] io::Error),
+}
