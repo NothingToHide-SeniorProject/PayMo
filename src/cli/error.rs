8ab43b47fcc631a58e80fea277e9654b0b19f0d4
Leonardo Nerone <leofnerone@proton.me>
2023-03-19 19:09:04 -0400

    update(cli): add Error and CmdError types

---
+++ src/cli/error.rs
@@ -0,0 +1,46 @@
+use crate::cli::Opts;
+use clap::CommandFactory;
+use std::{path::PathBuf, time};
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("File does not exist: {0}")]
+    FileNotFound(PathBuf),
+
+    #[error("Unsupported address: only regtest addresses are supported at the moment")]
+    UnsupportedAddress,
+
+    #[error(transparent)]
+    ParseInt(#[from] std::num::ParseIntError),
+
+    #[error("Invalid --time: must be greater than 10s, but {0:?} was provided")]
+    InvalidTime(time::Duration),
+
+    #[error(transparent)]
+    Cmd(#[from] CmdError),
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum CmdError {
+    #[error("Missing arguments: must use {0:?} with {1}")]
+    MissingArguments(Vec<String>, String),
+
+    #[error("Argument conflict: cannot use any of {0:?} with {1}")]
+    ArgumentConflict(Vec<String>, String),
+}
+
+impl From<CmdError> for clap::Error {
+    fn from(value: CmdError) -> Self {
+        use clap::error::ErrorKind;
+
+        let err_msg = value.to_string();
+        let mut cmd = Opts::command();
+
+        match value {
+            CmdError::MissingArguments(_, _) => {
+                cmd.error(ErrorKind::MissingRequiredArgument, err_msg)
+            }
+            CmdError::ArgumentConflict(_, _) => cmd.error(ErrorKind::ArgumentConflict, err_msg),
+        }
+    }
+}
