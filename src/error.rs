829c7fe096b17c5f2955152a286e3c91eab23f05
Leonardo Nerone <leofnerone@proton.me>
2023-03-19 19:10:01 -0400

    update(error.rs): update src/error.rs with new types

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,4 @@
+use crate::cli;
 use std::io;
 use thiserror::Error;
 
@@ -7,4 +8,7 @@ pub type Result<T> = std::result::Result<T, Error>;
 pub enum Error {
     #[error("I/O error: {0}")]
     Io(#[from] io::Error),
+
+    #[error("CLI error: {0}")]
+    Cli(#[from] cli::Error),
 }
