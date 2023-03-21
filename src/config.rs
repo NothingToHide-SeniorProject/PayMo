4bf81e2a5f3499b144c0abba6eaf6912d8485d69
Leonardo Nerone <leofnerone@proton.me>
2023-03-21 15:48:12 -0400

    update(config): update config.rs

--- src/config.rs
+++ src/config.rs
@@ -1,28 +1,20 @@
 use config::File;
-use log::{debug, info};
+use log::info;
 use serde::Deserialize;
 use std::path::Path;
 
-// TODO
-// pub const FARCASTER_INFO_SOCKET_NAME: &str = "{data_dir}/info";
-
 #[derive(Debug, Deserialize)]
-struct MoneroConfig {
-    daemon: String,
-    wallet_rpc: String,
+pub struct MoneroConfig {
+    pub daemon: String,
+    pub daemon_zmq: String,
+    pub wallet_rpc: String,
 }
 
-#[derive(Debug, Deserialize)]
-struct PaymoConfig {
-    bind_port: u16,
-    bind_ip: String,
-    monero: MoneroConfig,
-}
-
-// TODO add zmq sockets addr config
 #[derive(Debug, Deserialize)]
 pub struct Config {
-    paymo: PaymoConfig,
+    pub bind_port: u16,
+    pub bind_ip: String,
+    pub monero: MoneroConfig,
 }
 
 impl Config {
@@ -36,18 +28,7 @@ impl Config {
 
         let conf: Config = conf.try_deserialize().map_err(Into::<Error>::into)?;
 
-        debug!("{:#?}", conf);
-
-        Ok(Self {
-            paymo: PaymoConfig {
-                bind_port: 1234,
-                bind_ip: "0.0.0.0".to_string(),
-                monero: MoneroConfig {
-                    daemon: "".to_string(),
-                    wallet_rpc: "".to_string(),
-                },
-            },
-        })
+        Ok(conf)
     }
 }
 
